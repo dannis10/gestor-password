a29eb1eeccc98c153f16d582b5ea2613e20db28a
dannis10 <dansangar11@alu.edu.gva.es>
2023-11-18 14:51:19 -0500

    Añadido menú, falta depurar

--- gestor-pass.py
+++ gestor-pass.py
@@ -8,3 +8,22 @@ def main():
         print("1. Añadir contraseña")
         print("2. Listar contraseñas")
         print("3. Salir")
+
+        choice = input("Elige una opción: ")
+
+        if choice == "1":
+            password = input("Introduce la contraseña que deseas añadir: ")
+            add_password(password)
+            print("Contraseña añadida!!")
+        elif choice == "2":
+            print("\nContraseñas guardadas: ")
+            passwords = list_passwords()
+            for pw in passwords:
+                print(pw.strip())
+        elif choice == "3":
+            print("Saliendo del programa.")
+            break
+        else:
+            pirnt("Opción no válida. Por favor, selecciona una opción del menú.")
+if __name__ == "__main__":
+    main()
