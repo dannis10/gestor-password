007e35a888712d0b4b3aa932aba7d8c76f28b79b
dannis10 <dansangar11@alu.edu.gva.es>
2023-11-18 14:32:10 -0500

    creado archivo gestor-pass.py

---
+++ gestor-pass.py
@@ -0,0 +1,10 @@
+#1. Mostrar un menú al usuario con las siguientes opciones:
+#    Añadir contraseñas a un archivo.
+#    Listar contraseñas almacenadas.
+#    Salir del programa.
+def main():
+    while True:
+        print("\n--- Menú ---")
+        print("1. Añadir contraseña")
+        print("2. Listar contraseñas")
+        print("3. Salir")
